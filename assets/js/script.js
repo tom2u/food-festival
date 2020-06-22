f51d7abe35bd6dc5c5d72d796eb304c235448945
tom2u <tbrodhead@2u.com>
2020-06-22 15:21:34 -0500

    19.1.4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-require('bootstrap');
+import('./vendor/bootstrap.js');
 
 $(document).ready(function() {
 
@@ -8,6 +8,32 @@ $(document).ready(function() {
     const result = loremIpsum.split(/\s+/).slice(0,numWords).join(" ");
     return result;
   };
+
+  function createEl(htmlString, attrs, ...children) {
+    if (typeof htmlString !== "string") {
+      throw Error("Argument 'htmlString' is required and must be a string");
+    }
+    const el = document.createElement(htmlString);
+    if (typeof attrs === "object") {
+      for (let key in attrs) {
+        if (key.substring(0, 2) === "on") {
+          el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
+        } else {
+          el.setAttribute(key, attrs[key]);
+        }
+      }
+    }
+    children.forEach(function(child) {
+      let node;
+      if (child.constructor.name.includes("Element")) {
+        node = child;
+      } else {
+        node = document.createTextNode(child);
+      }
+      el.appendChild(node);
+    });
+    return el;
+  }
 
   if (window.location.href.indexOf("schedule") > -1) {
 
